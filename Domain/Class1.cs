483e80d2f7542373346050d094352a96d27cea5a
unknown <scottmacaz@yahoo.com>
2018-02-24 16:08:43 -0800

    changes to class1

--- Domain/Class1.cs
+++ Domain/Class1.cs
@@ -5,5 +5,11 @@ namespace gittest.Domain
     public class Class1
     {
         //some change
+        public string MyNewProp {get; set;}
+
+        public string WhosNumberOne()
+        {
+            "Scott, duh.";
+        }
     }
 }
