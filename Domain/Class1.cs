c8bab9efe310a573e205d1ea21c25ee045613bde
Franky Solano <fsolano@nevada.unr.edu>
2018-02-24 16:09:14 -0800

    added some props to Class1

--- Domain/Class1.cs
+++ Domain/Class1.cs
@@ -5,5 +5,13 @@ namespace gittest.Domain
     public class Class1
     {
         //some change
+        public string FooBar { get; set; }
+        public string GetterSetterForgetter { get; set; }
+        public string PleaseToMeetYaSeeYaWouldntWannaBeYa { get; set; }
+        public string Foo { get; set; }
+        
+        
+        
+        
     }
 }
